24c68128e57e48b39005b9d1ac0931f8307be426
A38954 ĐỖ MẠNH TÀI <a38954@thanglong.edu.vn>
2023-10-28 17:03:20 +0700

    Create database

---
+++ WebNews.Data/EF/WebNewsDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebNews.Data.Entities;
+
+namespace WebNews.Data.EF
+{
+    public class WebNewsDbContext : DbContext
+    {
+        public WebNewsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<News> Newss { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebNews.Data/Entities/News.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebNews.Data.Entities
+{
+    public class News
+    {
+        public int ID { get; set; }
+        public String Title { get; set; }
+        public String Describe { get; set; }
+        public String Category { get; set; }
+        public DateTime DateCreated { get; set; }
+        public Status Status { get; set; }
+        public int Image { get; set; }
+    }
+}
